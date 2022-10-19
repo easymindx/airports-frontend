3a7940d659de9255d15afef62c807965579d7acd
easymindx <sompaqueen47@gmail.com>
2022-10-19 09:45:35 +0900

    fix: collect types in one place

---
+++ src/types.d.ts
@@ -0,0 +1,25 @@
+type AirportType = {
+  airport_id: string;
+  airport_name: string;
+  city_iata_code: string;
+  country_iso2: string;
+  country_name: string;
+  geoname_id: string;
+  gmt: number;
+  iata_code: string;
+  icao_code: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  phone_number: string;
+  timezone: string;
+}
+
+interface Pagination {
+  limit?: number;
+  offset?: number;
+}
+
+interface QueryParams extends Pagination {
+  search?: string;
+}
