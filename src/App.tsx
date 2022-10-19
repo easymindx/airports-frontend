12780764ad71bef742bd39bd65c474c72d3416f8
easymindx <sompaqueen47@gmail.com>
2022-10-19 10:22:32 +0900

    fix: config for production

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <MainLayout>
       <Grid container spacing={4}>
-        <Grid item xs={12}>
+        <Grid item md={8} xs={12}>
           <Box sx={{ width: '100%', height: 400 }}>
             <GoogleMap
               zoom={DEFAULT_ZOOM}
@@ -30,8 +30,13 @@ function App() {
             />
           </Box>
         </Grid>
-        <Grid item xs={12}>
-          <AirportSelect airports={data} />
+        <Grid item container md={4} xs={12} spacing={4} sx={{ display: 'block' }}>
+          <Grid item xs={12}>
+            <AirportSelect airports={data} label="Choose origin airport" />
+          </Grid>
+          <Grid item xs={12}>
+            <AirportSelect airports={data} label="Choose destination airport" />
+          </Grid>
         </Grid>
       </Grid>
     </MainLayout>
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
-export const AERO_API_ENDPOINT = '/aeroapi';
-export const AVIATIONSTACK_API_ENDPOINT = '/aviationstack/v1'
+export const IS_PROD = process.env.NODE_ENV === 'production';
+export const AERO_API_ENDPOINT = IS_PROD ? 'https://montauk-cors.herokuapp.com/https://aeroapi.flightaware.com/aeroapi/' : '/aeroapi';
+export const AVIATIONSTACK_API_ENDPOINT = IS_PROD ? 'http://api.aviationstack.com/v1/' : '/aviationstack/v1';
