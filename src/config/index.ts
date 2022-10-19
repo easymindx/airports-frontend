95e2236debd419138af76e9a42917795f4bea047
easymindx <sompaqueen47@gmail.com>
2022-10-19 10:27:58 +0900

    fix: aviation api to use cors-proxy for avoiding https protection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 export const IS_PROD = process.env.NODE_ENV === 'production';
 export const AERO_API_ENDPOINT = IS_PROD ? 'https://montauk-cors.herokuapp.com/https://aeroapi.flightaware.com/aeroapi/' : '/aeroapi';
-export const AVIATIONSTACK_API_ENDPOINT = IS_PROD ? 'http://api.aviationstack.com/v1/' : '/aviationstack/v1';
+export const AVIATIONSTACK_API_ENDPOINT = IS_PROD ? 'https://montauk-cors.herokuapp.com/http://api.aviationstack.com/v1/' : '/aviationstack/v1';
